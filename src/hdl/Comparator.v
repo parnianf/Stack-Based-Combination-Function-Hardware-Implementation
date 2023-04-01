9d4f046603f69c8e9ff033e7278e36027d76632c
Parnian Fazel <69076293+parnianf@users.noreply.github.com>
2023-04-02 02:56:31 +0330

    Delete src/hdl directory

--- src/hdl/Comparator.v
+++
@@ -1,5 +0,0 @@
-`timescale 1ns/1ns
-
-module Comparator(input[63:0] in0, in1, output gt);
-  assign gt = in0 > in1;
-endmodule
--- src/hdl/Shifter.v
+++
@@ -1,5 +0,0 @@
-`timescale 1ns/1ns
-
-module Shifter1bit(input[63:0] in, output[63:0] out);
-  assign out = {1'b0, in[63:1]};
-endmodule
